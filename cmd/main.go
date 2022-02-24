b97871b102b25e41619ffdbba9d12ab6f0acb5bc
Piyush Katira <piyush@katirap-a01.vmware.com>
2022-02-24 10:58:46 +0400

    chg msg

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {	
 		log.Printf("unknown host")
 	}
-	resp := fmt.Sprintf("Hello Salah from Piyush!!")
+	resp := fmt.Sprintf("Testing!!")
 
 	_, err = w.Write([]byte(resp))
 	if err != nil {
