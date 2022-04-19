63a039ca812d6027a40c2c3c53415b758004ba38
piyush7 <piyush.katira.7@gmail.com>
2022-04-19 16:24:25 +0530

    Update main.go
    updated the msg

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {	
 		log.Printf("unknown host")
 	}
-	resp := fmt.Sprintf("Hello  KFU team!!")
+	resp := fmt.Sprintf("Hello  KFU team from vmware!!!")
 
 	_, err = w.Write([]byte(resp))
 	if err != nil {
