1437fe2e341e77060bcb21137cd5cfeeb025bddc
Piyush Katira <piyush@katirap-a01.vmware.com>
2022-07-04 18:10:56 +0530

    made changes to the code

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {	
 		log.Printf("unknown host")
 	}
-	resp := fmt.Sprintf("Hello  KFU team from vmware!!!")
+	resp := fmt.Sprintf("Hello  all !!")
 
 	_, err = w.Write([]byte(resp))
 	if err != nil {
