b79b703c03738910d5b89e67bee1c965ad62ce43
Piyush Katira <piyush@katirap-a01.vmware.com>
2022-07-13 17:17:22 +0530

    changed the message to hello team

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {	
 		log.Printf("unknown host")
 	}
-	resp := fmt.Sprintf("Hello  Harbor123!!!")
+	resp := fmt.Sprintf("Hello  team!!!")
 
 	_, err = w.Write([]byte(resp))
 	if err != nil {
